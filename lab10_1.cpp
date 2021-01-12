8eb0a301c39a318d3cd63f53e7613f8da4101d5b
peela123 <peelaxv@gmail.com>
2021-01-12 18:39:34 +0700

    งานlab10

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,37 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+string grade(int x)
+{
+    if (x == 0)
+        return "A";
+    else if (x == 1)
+        return "B+";
+    else if (x == 2)
+        return "B";
+    else if (x == 3)
+        return "C+";
+    else if (x == 4)
+        return "C";
+    else if (x == 5)
+        return "D+";
+    else if (x == 6)
+        return "D";
+    else if (x == 7)
+        return "F";
+    else
+        return "W";
+}
+int main()
+{
+    int x;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    x = rand() % 9;
+    cout << "You will get " << grade(x) << " in this 261102.";
+}
